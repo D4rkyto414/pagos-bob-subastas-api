1b49b9148b9308c3bcb2fed8fccb6dfcae5ccec3
Adriano Paul <adrianopaulperez2005@gmail.com>
2025-09-10 19:45:19 -0500

    Update jwt.js

--- backend/src/utils/jwt.js
+++ backend/src/utils/jwt.js
@@ -1,1 +1,9 @@
+const jwt = require("jsonwebtoken");
+const { JWT_SECRET } = require("../config/env");
+
+function generateToken(payload) {
+  return jwt.sign(payload, JWT_SECRET, { expiresIn: "1d" });
+}
+
+module.exports = { generateToken };
 
