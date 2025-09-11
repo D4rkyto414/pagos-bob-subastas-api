f0375d612b58322ce33edac555d9edf1eab68b3b
Adriano Paul <adrianopaulperez2005@gmail.com>
2025-09-10 19:48:54 -0500

    Update payment.routes.js

--- backend/src/routes/payment.routes.js
+++ backend/src/routes/payment.routes.js
@@ -1,1 +1,9 @@
+const express = require("express");
+const router = express.Router();
+const paymentController = require("../controllers/payment.controller");
+
+router.get("/", paymentController.getPayments);
+router.post("/", paymentController.createPayment);
+
+module.exports = router;
 
