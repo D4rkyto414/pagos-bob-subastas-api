c164ec671bfe42a4c86c581a09817280316ae4a5
Adriano Paul <adrianopaulperez2005@gmail.com>
2025-09-10 19:44:54 -0500

    Update error.middleware.js

--- backend/src/middlewares/error.middleware.js
+++ backend/src/middlewares/error.middleware.js
@@ -1,1 +1,7 @@
+function errorMiddleware(err, req, res, next) {
+  console.error("❌ Error:", err.message);
+  res.status(500).json({ error: "Error interno del servidor" });
+}
+
+module.exports = errorMiddleware;
 
