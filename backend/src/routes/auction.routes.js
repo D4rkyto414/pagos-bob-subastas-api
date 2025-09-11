3af5d267877e9be7b239286105f458ab8ffa34cb
Adriano Paul <adrianopaulperez2005@gmail.com>
2025-09-10 19:48:02 -0500

    Update auction.routes.js

--- backend/src/routes/auction.routes.js
+++ backend/src/routes/auction.routes.js
@@ -1,1 +1,9 @@
+const express = require("express");
+const router = express.Router();
+const auctionController = require("../controllers/auction.controller");
+
+router.get("/", auctionController.getAuctions);
+router.post("/", auctionController.createAuction);
+
+module.exports = router;
 
