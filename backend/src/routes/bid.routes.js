50069234fce9c1ff1db5514156f7b703f3ef0df6
Adriano Paul <adrianopaulperez2005@gmail.com>
2025-09-10 19:48:37 -0500

    Update bid.routes.js

--- backend/src/routes/bid.routes.js
+++ backend/src/routes/bid.routes.js
@@ -1,1 +1,9 @@
+const express = require("express");
+const router = express.Router();
+const bidController = require("../controllers/bid.controller");
+
+router.get("/", bidController.getBids);
+router.post("/", bidController.createBid);
+
+module.exports = router;
 
