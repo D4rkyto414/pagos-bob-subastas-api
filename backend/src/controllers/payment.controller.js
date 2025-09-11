9c2aa08f1c08ffcee330dbd024716a23a638d665
Adriano Paul <adrianopaulperez2005@gmail.com>
2025-09-10 19:46:32 -0500

    Update payment.controller.js

--- backend/src/controllers/payment.controller.js
+++ backend/src/controllers/payment.controller.js
@@ -1,1 +1,12 @@
+const prisma = require("../config/database");
+
+exports.getPayments = async (req, res) => {
+  const payments = await prisma.payment.findMany();
+  res.json(payments);
+};
+
+exports.createPayment = async (req, res) => {
+  const payment = await prisma.payment.create({ data: req.body });
+  res.json(payment);
+};
 
