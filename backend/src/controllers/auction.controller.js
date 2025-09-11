1eac2897b6f18a170c1690d6e4a79760b98fc949
Adriano Paul <adrianopaulperez2005@gmail.com>
2025-09-10 19:45:58 -0500

    Update auction.controller.js

--- backend/src/controllers/auction.controller.js
+++ backend/src/controllers/auction.controller.js
@@ -1,1 +1,12 @@
+const prisma = require("../config/database");
+
+exports.getAuctions = async (req, res) => {
+  const auctions = await prisma.auction.findMany();
+  res.json(auctions);
+};
+
+exports.createAuction = async (req, res) => {
+  const auction = await prisma.auction.create({ data: req.body });
+  res.json(auction);
+};
 
