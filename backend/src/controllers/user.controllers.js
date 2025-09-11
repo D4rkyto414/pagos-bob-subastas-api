c10b7d972d2ea05e347e4b473dd9494d0af34392
Adriano Paul <adrianopaulperez2005@gmail.com>
2025-09-10 19:45:45 -0500

    Update user.controllers.js

--- backend/src/controllers/user.controllers.js
+++ backend/src/controllers/user.controllers.js
@@ -1,1 +1,12 @@
+const prisma = require("../config/database");
+
+exports.getUsers = async (req, res) => {
+  const users = await prisma.user.findMany();
+  res.json(users);
+};
+
+exports.createUser = async (req, res) => {
+  const user = await prisma.user.create({ data: req.body });
+  res.json(user);
+};
 
