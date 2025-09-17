f94be48e5d1a5634928ebe06ba1c1e1e35a433fb
Adriano Paul <adrianopaulperez2005@gmail.com>
2025-09-17 13:13:24 -0500

    Delete backend directory

--- backend/src/config/env.js
+++
@@ -1,8 +0,0 @@
-require("dotenv").config();
-
-module.exports = {
-  PORT: process.env.PORT,
-  DATABASE_URL: process.env.DATABASE_URL,
-  JWT_SECRET: process.env.JWT_SECRET,
-};
-
--- backend/src/routes/auction.routes.js
+++
@@ -1,9 +0,0 @@
-const express = require("express");
-const router = express.Router();
-const auctionController = require("../controllers/auction.controller");
-
-router.get("/", auctionController.getAuctions);
-router.post("/", auctionController.createAuction);
-
-module.exports = router;
-
