8a5ba340ed9d8f9b884132a72c5ae81a03084942
Adriano Paul <adrianopaulperez2005@gmail.com>
2025-09-17 15:08:02 -0500

    Update api.js

--- src/services/api.js
+++ src/services/api.js
@@ -1,1 +1,20 @@
+import axios from 'axios';
 
+const api = axios.create({
+  baseURL: 'https://api.pagos-bob-subastas.com/v1', // Reemplazar con la URL real de tu API
+  headers: {
+    'Content-Type': 'application/json',
+  },
+});
+
+api.interceptors.request.use((config) => {
+  const token = localStorage.getItem('token');
+  if (token) {
+    config.headers.Authorization = `Bearer ${token}`;
+  }
+  return config;
+}, (error) => {
+  return Promise.reject(error);
+});
+
+export default api;
